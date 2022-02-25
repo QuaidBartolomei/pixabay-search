237d2dc118f0ab4afa207ce73835dc3e709f733b
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-02-25 01:27:00 -0800

    feat: add images api route to nextjs server

---
+++ pages/api/images.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+const key = process.env.API_KEY
+const host = 'https://pixabay.com/api/'
+
+export default async function endpoint(
+  request: NextApiRequest,
+  response: NextApiResponse,
+): Promise<void> {
+  const {
+    query: { q },
+  } = request
+
+  try {
+    const res = await axios.get(host, { params: { key, q } })
+    console.log(key, q, host)
+    return response.status(res.status).json(res.data)
+  } catch (err) {
+    return response.status(400).send(err)
+  }
+}

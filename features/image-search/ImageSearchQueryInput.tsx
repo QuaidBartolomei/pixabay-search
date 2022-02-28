3f89354ed4ee15bcf17247794f750457c9d687a6
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2022-02-28 02:03:07 -0800

    feat: query input uses debouncing to limit api calls

--- features/image-search/ImageSearchQueryInput.tsx
+++ features/image-search/ImageSearchQueryInput.tsx
@@ -1,33 +1,42 @@
-import Box from '@mui/material/Box'
+import { Stack } from '@mui/material'
 import TextField from '@mui/material/TextField'
-import { useDispatch, useSelector } from 'react-redux'
-import { selectImageSearchQuery } from 'app/store'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import ImageSearchButton from './ImageSearchButton'
 import { setQuery } from './imageSearchSlice'
 
+const delay = 500
+
 export default function ImageSearchQueryInput() {
-  const query = useSelector(selectImageSearchQuery)
   const dispatch = useDispatch()
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      dispatch(setQuery(value))
+    }, delay)
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [dispatch, value])
 
   return (
-    <Box
+    <Stack
+      direction="row"
+      justifyContent="center"
       sx={{
         m: 1,
         width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
       }}
     >
       <TextField
         id="outlined-basic"
         label="Outlined"
         variant="outlined"
-        value={query}
-        onChange={(e) => {
-          dispatch(setQuery(e.target.value))
-        }}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
-    </Box>
+      <ImageSearchButton />
+    </Stack>
   )
 }
